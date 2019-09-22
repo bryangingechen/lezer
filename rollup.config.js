ab3ee53369601765d3cf76ee629005008508c849
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2019-09-22 14:54:36 -0400

    umd build

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import typescript from "rollup-plugin-typescript2"
 import nodeResolve from "rollup-plugin-node-resolve"
 import commonJS from "rollup-plugin-commonjs"
 
-export default {
+export default [{
   input: "./src/index.ts",
   external: ["lezer-tree"],
   output: {
@@ -28,4 +28,32 @@ export default {
       include: ["src/*.ts"]
     })
   ]
-}
+},
+{
+  input: "./src/index.ts",
+  // external: ["lezer-tree"],
+  output: {
+    name: "lezer",
+    format: "umd",
+    file: "./dist/index.umd.js",
+    sourcemap: true,
+    externalLiveBindings: false
+  },
+  plugins: [
+    nodeResolve(),
+    commonJS(),
+    typescript({
+      check: false,
+      tsconfigOverride: {
+        compilerOptions: {
+          lib: ["es5", "es6"],
+          sourceMap: true,
+          target: "es5",
+          strict: false,
+          declaration: true
+        }
+      },
+      include: ["src/*.ts"]
+    })
+  ]
+}]
